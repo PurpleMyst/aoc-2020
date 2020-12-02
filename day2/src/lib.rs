22a36a3ea21d9c5a5229620b30c607b4ef240d50
PurpleMyst <PurpleMyst@users.noreply.github.com>
2020-12-02 10:53:04 +0100

    refactor(day2): use Itertools::collect_tuple

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -1,20 +1,22 @@
+use itertools::Itertools as _;
+
 #[inline]
 pub fn solve() -> (usize, usize) {
     let mut part1 = 0;
     let mut part2 = 0;
 
     include_str!("input.txt").lines().for_each(|line| {
-        let mut parts = line.splitn(3, ' ');
-        let range = parts.next().unwrap();
-        let letter = parts.next().unwrap().chars().next().unwrap() as u8;
-        let password = parts.next().unwrap().as_bytes();
+        let (range, letter, password) = line.splitn(3, ' ').collect_tuple().unwrap();
+        let letter = letter.chars().next().unwrap() as u8;
+        let password = password.as_bytes();
 
-        let mut range_parts = range.splitn(2, '-');
-        let low = range_parts.next().unwrap().parse::<usize>().unwrap();
-        let high = range_parts.next().unwrap().parse::<usize>().unwrap();
+        let (low, high) = range
+            .splitn(2, '-')
+            .map(|bound| bound.parse::<usize>().unwrap())
+            .collect_tuple()
+            .unwrap();
 
         let present = password.iter().filter(|&&c| c == letter).count();
-
         if present >= low && present <= high {
             part1 += 1;
         }
