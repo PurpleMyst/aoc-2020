4bb331376b2c18823e4f0709eb5e13173d5d3d6f
PurpleMyst <PurpleMyst@users.noreply.github.com>
2020-12-06 18:22:07 +0100

    bench: change criterion config

--- benchmark/benches/benchmark.rs
+++ benchmark/benches/benchmark.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use criterion::{criterion_group, criterion_main, Criterion};
 
 pub fn day1_benchmark(c: &mut Criterion) {
@@ -41,7 +43,14 @@ pub fn alldays_benchmark(c: &mut Criterion) {
 
 criterion_group! {
     name = benches;
-    config = Criterion::default().significance_level(0.1).sample_size(500);
+
+    config = Criterion::default()
+        .significance_level(0.1)
+        .sample_size(500)
+        .measurement_time(Duration::from_secs(30))
+        .warm_up_time(Duration::from_secs(15))
+        .noise_threshold(0.05);
+
     targets =
         day1_benchmark,
         day2_benchmark,
