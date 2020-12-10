a0e88d661efdfb174208eddb47430028ddf94dd7
PurpleMyst <PurpleMyst@users.noreply.github.com>
2020-12-10 11:56:33 +0100

    perf(day10): implement smarter solutions
    part1 solved in O(n)
    part2 solved in O(n)
    with O(log n) for sorting
    
    i am
    v tired
    
    i'd had a hunch about the part 2 solution about the edges but i went with the
    weirder solution just cause it seemed simpler to implement
    it wasn't
    
    part1's smart solution I hadn't thought of at all.
    
    oh well.
    should've slept more and I would've come up with a smarter solution the first time round. sigh.

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -1,192 +1,45 @@
-use std::collections::BinaryHeap;
+fn solve_part1(adapters: &[u8]) -> usize {
+    let mut ones = 0;
+    let mut threes = 0;
 
-#[derive(Ord, PartialOrd, Eq, PartialEq, Debug, Copy, Clone)]
-struct Node {
-    length: usize,
-    jolts: u8,
-    deltas: [usize; 3],
+    for (a, b) in adapters.iter().zip(adapters.iter().skip(1)) {
+        match b - a {
+            1 => ones += 1,
+            3 => threes += 1,
+            _ => {}
+        }
+    }
+
+    ones * threes
 }
 
-fn solve_part1(adapters: &[bool; 256], highest: u8) -> Node {
-    let mut q = BinaryHeap::new();
+fn solve_part2(adapters: &[u8]) -> u64 {
+    let mut edges = [0; 256];
+    edges[0] = 1;
+    edges[1] = 1;
+    edges[2] = 2;
+    debug_assert!(adapters.contains(&0) && adapters.contains(&1) && adapters.contains(&2));
 
-    q.push(Node {
-        length: 0,
-        jolts: highest,
-        deltas: [0; 3],
-    });
-
-    while let Some(node) = q.pop() {
-        if node.jolts == 0 {
-            return node;
-        }
-
-        q.extend((1..=3).filter_map(|delta| {
-            if node.jolts < delta || !adapters[(node.jolts - delta) as usize] {
-                return None;
-            }
-
-            let jolts = node.jolts - delta;
-            let length = node.length + 1;
-            let mut deltas = node.deltas;
-            deltas[(delta - 1) as usize] += 1;
-            let node = Node {
-                length,
-                jolts,
-                deltas,
-            };
-
-            Some(node)
-        }));
+    for &n in adapters.iter().skip(3) {
+        edges[n as usize] =
+            edges[(n - 1) as usize] + edges[(n - 2) as usize] + edges[(n - 3) as usize];
     }
 
-    unreachable!()
-}
-
-fn count_walks(adapters: &[bool; 256], highest: u8, target: u8) -> u64 {
-    let mut q = BinaryHeap::new();
-
-    q.push(Node {
-        length: 0,
-        jolts: highest,
-        deltas: [0; 3],
-    });
-
-    let mut total = 0;
-    while let Some(node) = q.pop() {
-        if node.jolts == target {
-            total += 1;
-        }
-
-        q.extend((1..=3).filter_map(|delta| {
-            if node.jolts < delta || !adapters[(node.jolts - delta) as usize] {
-                return None;
-            }
-
-            let jolts = node.jolts - delta;
-            let length = node.length + 1;
-            let mut deltas = node.deltas;
-            deltas[(delta - 1) as usize] += 1;
-            let node = Node {
-                length,
-                jolts,
-                deltas,
-            };
-
-            Some(node)
-        }));
-    }
-
-    total
-}
-
-struct Solver {
-    adapters: [bool; 256],
-    block: Vec<u8>,
-    walks: u64,
-}
-
-impl Solver {
-    fn new(adapters: [bool; 256]) -> Self {
-        Self {
-            adapters,
-            block: vec![],
-            walks: 1,
-        }
-    }
-
-    fn walk(&mut self) {
-        let mut jolts = 0;
-
-        loop {
-            self.block.clear();
-
-            self.next_complex_block(jolts);
-
-            if self.block.is_empty() {
-                if let Some(next_jolts) = (1..=3)
-                    .map(|delta| jolts + delta)
-                    .find(|&next_jolts| self.adapters[next_jolts as usize])
-                {
-                    jolts = next_jolts;
-                    continue;
-                } else {
-                    break;
-                }
-            }
-
-            self.block.insert(0, jolts);
-            jolts = *self.block.last().unwrap();
-
-            self.walks *= self.current_block_walks(jolts);
-        }
-    }
-
-    fn current_block_walks(&mut self, start: u8) -> u64 {
-        let mut adapters = [false; 256];
-
-        self.block.iter().for_each(|&n| adapters[n as usize] = true);
-
-        let target = *self.block.first().unwrap();
-
-        count_walks(&adapters, start as u8, target as u8)
-    }
-
-    fn next_complex_block(&mut self, start: u8) {
-        let mut next = 0u8;
-
-        for delta in 1..=3 {
-            if self.adapters[(start + delta) as usize] {
-                next |= 1 << delta;
-            }
-        }
-
-        if next.count_ones() <= 1 {
-            return;
-        }
-
-        for delta in 1..=3 {
-            if self.adapters[(start + delta) as usize] {
-                match self.block.binary_search(&(start + delta)) {
-                    Ok(..) => {}
-                    Err(idx) => self.block.insert(idx, start + delta),
-                }
-
-                self.next_complex_block(start + delta);
-            }
-        }
-    }
-}
-
-fn solve_part2(adapters: [bool; 256]) -> u64 {
-    let mut solver = Solver::new(adapters);
-    solver.walk();
-
-    solver.walks
+    edges[*adapters.iter().max().unwrap() as usize]
 }
 
 #[inline]
 pub fn solve() -> (usize, u64) {
-    let mut adapters = [false; 256];
-
-    let highest = include_str!("input.txt")
+    let mut adapters = include_str!("input.txt")
         .lines()
         .map(|n| n.parse::<u8>().unwrap())
-        .inspect(|&n| adapters[n as usize] = true)
-        .max()
-        .unwrap();
+        .collect::<Vec<_>>();
+    adapters.sort();
+    adapters.insert(0, 0);
+    adapters.push(adapters.last().unwrap() + 3);
 
-    assert!(adapters[highest as usize]);
-
-    let highest = highest + 3;
-    adapters[highest as usize] = true;
-    adapters[0] = true;
-
-    let node = solve_part1(&adapters, highest);
-
-    let part1 = node.deltas[0] * node.deltas[2];
-
-    let part2 = solve_part2(adapters);
+    let part1 = solve_part1(&adapters);
+    let part2 = solve_part2(&adapters);
 
     (part1, part2)
 }
